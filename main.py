fe40123d4ee8bef3141f25d9989013ef78db65cc
alvarohurtadobo <a.hurtado.bo@gmail.com>
2019-05-14 22:06:13 -0400

    Added small improvements in background, to be tested why the FPS slows down

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import argparse
 import numpy as np 
 from time import time
 
+from background import Background
+from tools import rectangle_already_tracked
+
 import kcftracker
 
-parser = argparse.ArgumentParser(description='Introduce arguments to run the tracker')
+parser = argparse.ArgumentParser(description = 'Introduce arguments to run the tracker')
 parser.add_argument('-i', '--input',  type = str, default = None, help = 'Introduce the input video to analyse')
 parser.add_argument('-W', '--width',  type = int, default = 0, help = 'Introduce the desired frame width')
 parser.add_argument('-H', '--height', type = int, default = 0, help = 'Introduce the desired frame height')
@@ -62,28 +65,52 @@ if __name__ == '__main__':
 
 	tracker = kcftracker.KCFTracker(True, True, True)  # hog, fixed_window, multiscale
 	#if you use hog feature, there will be a short pause after you draw a first boundingbox, that is due to the use of Numba.
+	ret, frame = cap.read()
+	myBackground = Background(frame)
 
 	cv2.namedWindow('tracking',cv2.WINDOW_NORMAL)
 	cv2.setMouseCallback('tracking',draw_boundingbox)
 
 	while(cap.isOpened()):
 		ret, frame = cap.read()
+		new_objects = myBackground.detect(frame)
+		sure_objects = [my_object['box'] for my_object in new_objects if my_object['confidence']>0.6]
 
+		most_objects = sure_objects
+		"""
+		last_area = 0
+		for rectangle in sure_objects:
+			_, _, w, h = rectangle
+			new_area = w*h
+			if new_area > last_area:
+				last_area = new_area
+				most_objects.append(rectangle)
+			if len(most_objects)
+		"""
 		# If stated we change the working frame resolution
 		if args.width and args.height:
 			frame = cv2.resize(frame,(args.width,args.height))
 		if not ret:
 			break
 
+		if most_objects != []:
+			for rectangle in most_objects:
+				cv2.rectangle(frame,(ix,iy), (ix+w,iy+h), (0,255,255), 2)
+				if rectangle_already_tracked([tracker._roi],rectangle):
+					print('Already tracked',[tracker._roi],rectangle)
+				else:
+					ix, iy, w, h = rectangle
+					print('Rectangle problem: ',rectangle)
+					cv2.rectangle(frame,(ix,iy), (ix+w,iy+h), (0,255,255), 4)
+					tracker.init([ix,iy,w,h], frame)
+					print('Not tracked')
+					onTracking = True
+
 		if(selectingObject):
-			print((ix,iy), (cx,cy))
 			cv2.rectangle(frame,(ix,iy), (cx,cy), (0,255,255), 1)
 		elif(initTracking):
-			print((ix,iy), (ix+w,iy+h))
 			cv2.rectangle(frame,(ix,iy), (ix+w,iy+h), (0,255,255), 2)
-
 			tracker.init([ix,iy,w,h], frame)
-
 			initTracking = False
 			onTracking = True
 		elif(onTracking):
---
+++ tools.py
@@ -0,0 +1,38 @@
+"""
+Some tools
+
+"""
+
+def rectangle_percentage_coincidence(reference_rectangle, rectangle):
+    # The rectangles must be in the form: x, y, w, h
+    # The next three lines calculate the overlap area between the two rectangles:
+    # Reference rectangle:
+    best_x_min, best_y_min = reference_rectangle[0], reference_rectangle[1]
+    best_x_max, best_y_max = best_x_min + reference_rectangle[2], best_y_min + reference_rectangle[3]
+    reference_area = (best_x_max - best_x_min) * (best_y_max - best_y_min)
+
+    # Overlap rectangle:
+    x0, y0, x1, y1 =  rectangle[0], rectangle[1], rectangle[0] + rectangle[2], rectangle[1]+ rectangle[3]
+    x_overlap = max(0, min(x1, best_x_max) - max(x0, best_x_min))
+    y_overlap = max(0, min(y1, best_y_max) - max(y0, best_y_min))
+    overlapArea = x_overlap * y_overlap
+    if reference_area == 0:
+        return 0
+    coincidence = overlapArea/reference_area
+    #print('Overlap area: {}/ Reference: {} = {}'.format(overlapArea,reference_area,coincidence))
+    return coincidence
+
+def tracker_already_tracked(trackers, rectangle):
+    for tracker in trackers:
+        if rectangle_percentage_coincidence(tracker._roi, rectangle) > 0.7:
+            return True 
+    return False
+
+def rectangle_already_tracked(rectangles, rectangle):
+    """
+    The rectangle comes in the form x, y, w, h
+    """
+    for current_rectangle in rectangles:
+        if rectangle_percentage_coincidence(current_rectangle, rectangle) > 0.7:
+            return True 
+    return False
